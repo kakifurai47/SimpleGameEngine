834cb6e3053a4e1a12d6fa65f81a11d4bf426b8e
kakifurai <87513233+kakifurai47@users.noreply.github.com>
2022-06-02 18:26:33 +0800

    compiled shader update

--- examples/Test101/LocalTemp/Imported/Assets/Shaders/test.shader/code.hlsl
+++ examples/Test101/LocalTemp/Imported/Assets/Shaders/test.shader/code.hlsl
@@ -35,30 +35,13 @@ struct PixelIn {
 float4x4	SGE_MVP;
 
 float  x;
-double b;
 float4 p;
 float4 v;
-
-cbuffer VSConstants {
-	float4x4 WVPMatrix1;
-	float4x4 WVPMatrix2;
-}
-
-cbuffer VSConstants2 {
-	float4x4 WVPMatrix3;
-	float4x4 WVPMatrix4;
-}
-
 
 PixelIn vs_main(VertexIn i) {
     PixelIn o;	
     o.position = i.position;
-	o.position = p;
     o.color    = i.color;
-	o.color.x  = x;
-	o.color.y  = v.y;
-	o.color.z  = WVPMatrix1._m00;
-	o.color.w  = WVPMatrix3._m02;
     return o;
 }
 
